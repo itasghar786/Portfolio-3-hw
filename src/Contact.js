31753971aaef35015efe0728f1a29dcda5ce1af6
asghar sheikh <itasghar786@gmail.com>
2020-05-26 02:12:51 -0400

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export const Contact = () => (
+
+    <div>
+        <h2>Contact Us</h2>
+        <p>fifbdsuvbdukvbou</p>
+        
+        <p>bfgyjgfukegvbkuevbebvebvbuevewkvbevbevewuvewuvewenvenvne</p>
+    </div>
+)
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () =>(
+
+    <div>
+        <h2> Hello </h2>
+        <p>Hi I'm Asghar. I am a very hard working individual. My motto in life is that hard work can over come any obsticles.
+                               Programming has been my passion and now I am fullfilling my dream as I face the mystic world of programming.
+                              With my enthusiasm and devotion to programming, I believe that I can take my creativity to soaring heights.</p>
+                              </div>
+                              );
