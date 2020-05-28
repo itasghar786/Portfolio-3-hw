d544d5ab6d85863083c213e059ab4eeb8ae3f762
asghar sheikh <itasghar786@gmail.com>
2020-05-27 21:26:25 -0400

    Updates

---
+++ precache-manifest.6eef011ecd558aa35b8d59cfda7651e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf60199d6cdb9f7cdcca030ca04dc056",
+    "url": "/portfolio-3-hw/index.html"
+  },
+  {
+    "revision": "df0830b0010dd3962ad8",
+    "url": "/portfolio-3-hw/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10655f1099d27f962a4b",
+    "url": "/portfolio-3-hw/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df0830b0010dd3962ad8",
+    "url": "/portfolio-3-hw/static/js/2.9b38b894.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-3-hw/static/js/2.9b38b894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10655f1099d27f962a4b",
+    "url": "/portfolio-3-hw/static/js/main.17f8e080.chunk.js"
+  },
+  {
+    "revision": "894bfdbd51a95712972f",
+    "url": "/portfolio-3-hw/static/js/runtime-main.779f6f2b.js"
+  },
+  {
+    "revision": "5f1f0b7b32f86957f1101ecaabd1cb19",
+    "url": "/portfolio-3-hw/static/media/DaySch.5f1f0b7b.png"
+  },
+  {
+    "revision": "184d12cdf8ec5eca9325d94e7d20d241",
+    "url": "/portfolio-3-hw/static/media/boatImage.184d12cd.jpg"
+  }
+]);
